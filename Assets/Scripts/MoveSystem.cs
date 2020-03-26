0009f74c3b9720afd4b9d7e7e7c7c30b0cb079f6
issacstinson <62465740+issacstinson@users.noreply.github.com>
2020-03-26 18:47:34 +0900

    STOP
    Raycast 완성

--- Assets/Scripts/MoveSystem.cs
+++ Assets/Scripts/MoveSystem.cs
@@ -5,29 +5,27 @@ using UnityEngine;
 public class MoveSystem : MonoBehaviour
 {
     private PolygonCollider2D polygonCollider;
+    
     public LayerMask layerMask;
     public float movementSpeed = 6.25f;
     Rigidbody2D rbody;
 
     private void Start()
     {
-                
-    }
-
-    private void Awake()
-    {
         rbody = GetComponent<Rigidbody2D>();
         polygonCollider = GetComponent<PolygonCollider2D>();
-        RaycastHit2D hit;
+        
+    }
 
-        Vector2 start = transform.position;
+    void Awake()
+    {
 
-        Vector2 end = start + new Vector2;
     }
     // Update is called once per frame
 
     void Update()
     {
+        RaycastHit2D hit;
         Vector2 currentPos = rbody.position;
         if (Input.GetKey(KeyCode.D) == true)
         {
@@ -36,14 +34,31 @@ public class MoveSystem : MonoBehaviour
                 Vector2 inputVector = new Vector2(2, 1);
                 Vector2 movement = inputVector * movementSpeed;
                 Vector2 newPos = currentPos + movement * Time.fixedDeltaTime;
-                rbody.MovePosition(newPos);
+                Vector2 sight = inputVector * movementSpeed *2;
+                Vector2 chkPos = currentPos + sight * Time.fixedDeltaTime;
+                polygonCollider.enabled = false;
+                hit = Physics2D.Raycast(newPos, chkPos, layerMask);
+                polygonCollider.enabled = true;
+                if (hit.transform.tag == "Path")
+                {
+                    rbody.MovePosition(newPos);
+                }
+            
             }
             if (Input.GetKeyDown(KeyCode.S) == true)
             {
                 Vector2 inputVector = new Vector2(2, -1);
                 Vector2 movement = inputVector * movementSpeed;
                 Vector2 newPos = currentPos + movement * Time.fixedDeltaTime;
-                rbody.MovePosition(newPos);
+                Vector2 sight = inputVector * movementSpeed * 2;
+                Vector2 chkPos = currentPos + sight * Time.fixedDeltaTime;
+                polygonCollider.enabled = false;
+                hit = Physics2D.Raycast(newPos, chkPos, layerMask);
+                polygonCollider.enabled = true;
+                if (hit.transform.tag == "Path")
+                {
+                    rbody.MovePosition(newPos);
+                }
             }
         }
         if (Input.GetKey(KeyCode.A) == true)
@@ -53,16 +68,33 @@ public class MoveSystem : MonoBehaviour
                 Vector2 inputVector = new Vector2(-2, 1);
                 Vector2 movement = inputVector * movementSpeed;
                 Vector2 newPos = currentPos + movement * Time.fixedDeltaTime;
-                rbody.MovePosition(newPos);
+                Vector2 sight = inputVector * movementSpeed * 2;
+                Vector2 chkPos = currentPos + sight * Time.fixedDeltaTime;
+                polygonCollider.enabled = false;
+                hit = Physics2D.Raycast(newPos, chkPos, layerMask);
+                polygonCollider.enabled = true;
+                if (hit.transform.tag == "Path")
+                {
+                    rbody.MovePosition(newPos);
+                }
             }
             if (Input.GetKeyDown(KeyCode.S) == true)
             {
                 Vector2 inputVector = new Vector2(-2, -1);
                 Vector2 movement = inputVector * movementSpeed;
                 Vector2 newPos = currentPos + movement * Time.fixedDeltaTime;
-                rbody.MovePosition(newPos);
+                Vector2 sight = inputVector * movementSpeed * 2;
+                Vector2 chkPos = currentPos + sight * Time.fixedDeltaTime;
+                polygonCollider.enabled = false;
+                hit = Physics2D.Raycast(newPos, chkPos, layerMask);
+                polygonCollider.enabled = true;
+                if (hit.transform.tag == "Path")
+                {
+                    rbody.MovePosition(newPos);
+                }
             }
         }
+        
         if (Input.GetKey(KeyCode.W) == true)
         {
             if (Input.GetKeyDown(KeyCode.D) == true)
@@ -70,14 +102,30 @@ public class MoveSystem : MonoBehaviour
                 Vector2 inputVector = new Vector2(2, 1);
                 Vector2 movement = inputVector * movementSpeed;
                 Vector2 newPos = currentPos + movement * Time.fixedDeltaTime;
-                rbody.MovePosition(newPos);
+                Vector2 sight = inputVector * movementSpeed * 2;
+                Vector2 chkPos = currentPos + sight * Time.fixedDeltaTime;
+                polygonCollider.enabled = false;
+                hit = Physics2D.Raycast(newPos, chkPos, layerMask);
+                polygonCollider.enabled = true;
+                if (hit.transform.tag == "Path")
+                {
+                    rbody.MovePosition(newPos);
+                }
             }
             if (Input.GetKeyDown(KeyCode.A) == true)
             {
                 Vector2 inputVector = new Vector2(-2, 1);
                 Vector2 movement = inputVector * movementSpeed;
                 Vector2 newPos = currentPos + movement * Time.fixedDeltaTime;
-                rbody.MovePosition(newPos);
+                Vector2 sight = inputVector * movementSpeed * 2;
+                Vector2 chkPos = currentPos + sight * Time.fixedDeltaTime;
+                polygonCollider.enabled = false;
+                hit = Physics2D.Raycast(newPos, chkPos, layerMask);
+                polygonCollider.enabled = true;
+                if (hit.transform.tag == "Path")
+                {
+                    rbody.MovePosition(newPos);
+                }
             }
         }
         if (Input.GetKey(KeyCode.S) == true)
@@ -87,14 +135,30 @@ public class MoveSystem : MonoBehaviour
                 Vector2 inputVector = new Vector2(-2, -1);
                 Vector2 movement = inputVector * movementSpeed;
                 Vector2 newPos = currentPos + movement * Time.fixedDeltaTime;
-                rbody.MovePosition(newPos);
+                Vector2 sight = inputVector * movementSpeed * 2;
+                Vector2 chkPos = currentPos + sight * Time.fixedDeltaTime;
+                polygonCollider.enabled = false;
+                hit = Physics2D.Raycast(newPos, chkPos, layerMask);
+                polygonCollider.enabled = true;
+                if (hit.transform.tag == "Path")
+                {
+                    rbody.MovePosition(newPos);
+                }
             }
             if (Input.GetKeyDown(KeyCode.D) == true)
             {
                 Vector2 inputVector = new Vector2(2, -1);
                 Vector2 movement = inputVector * movementSpeed;
                 Vector2 newPos = currentPos + movement * Time.fixedDeltaTime;
-                rbody.MovePosition(newPos);
+                Vector2 sight = inputVector * movementSpeed * 2;
+                Vector2 chkPos = currentPos + sight * Time.fixedDeltaTime;
+                polygonCollider.enabled = false;
+                hit = Physics2D.Raycast(newPos, chkPos, layerMask);
+                polygonCollider.enabled = true;
+                if (hit.transform.tag == "Path")
+                {
+                    rbody.MovePosition(newPos);
+                }
             }
         }
     }
